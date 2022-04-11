526f2920e3011d003958cedb50f220468a6adf46
essietom <essietom@gmail.com>
2022-04-11 10:47:31 +0300

    create helper functions and validators

--- utilities/utils.go
+++ utilities/utils.go
@@ -1,1 +1,80 @@
 package utilities
+
+import (
+	"encoding/json"
+	"github.com/go-playground/validator/v10"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"strings"
+)
+
+var validate *validator.Validate
+
+func ParseBodyTest(r *http.Request, x interface{}, w http.ResponseWriter) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			ErrorResponse(http.StatusUnprocessableEntity, "Invalid JSON", w)
+			return
+		}
+	}
+}
+
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
+}
+
+func ValidateInputs(dataSet interface{}) (bool, map[string][]string) {
+	validate = validator.New()
+
+	err := validate.Struct(dataSet)
+
+	if err != nil {
+
+		//Validation syntax is invalid
+		if err, ok := err.(*validator.InvalidValidationError); ok {
+			panic(err)
+		}
+
+		//Validation errors occurred
+		errors := make(map[string][]string)
+		//Use reflector to reverse engineer struct
+		reflected := reflect.ValueOf(dataSet)
+		for _, err := range err.(validator.ValidationErrors) {
+
+			//fmt.Println("structf", err.StructField())
+			//fmt.Println("refl", reflected.Type().Elem())
+
+			// Attempt to find field by name and get json tag name
+			field, _ := reflected.Type().Elem().FieldByName(err.StructField())
+			var name string
+
+			//If json tag doesn't exist, use lower case of name
+			if name = field.Tag.Get("json"); name == "" {
+				name = strings.ToLower(err.StructField())
+			}
+
+			switch err.Tag() {
+			case "required":
+				errors[name] = append(errors[name], "The "+name+" is required")
+				break
+			case "email":
+				errors[name] = append(errors[name], "The "+name+" should be a valid email")
+				break
+			case "eqfield":
+				errors[name] = append(errors[name], "The "+name+" should be equal to the "+err.Param())
+				break
+			default:
+				errors[name] = append(errors[name], "The "+name+" is invalid")
+				break
+			}
+		}
+
+		return false, errors
+	}
+	return true, nil
+}
