c4bfcf911ff543cf7f5933a9033e15d3f2ae3548
essietom <essietom@gmail.com>
2022-04-11 10:49:01 +0300

    go setup

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package Accessibility_Backend
+
+import (
+	"Accessibility-Backend/database"
+	"Accessibility-Backend/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	database.Setup()
+	route := mux.NewRouter()
+	routes.RegisterRoutes(route)
+	if err := http.ListenAndServe(":3000", route); err != nil {
+		log.Fatal(err)
+	}
+}
