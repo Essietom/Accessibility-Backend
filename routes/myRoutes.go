8bea42413a6f29e2ff2909d448c69ec2266d2475
essietom <essietom@gmail.com>
2022-04-11 10:49:26 +0300

    expose endpoints url

--- routes/myRoutes.go
+++ routes/myRoutes.go
@@ -1,1 +1,40 @@
 package routes
+
+import (
+	"Accessibility-Backend/controllers"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(rw).Encode(map[string]string{"data": "Hello from Mux & mongoDB"})
+	}).Methods("GET")
+	router.HandleFunc("/criteria/", controllers.CreateCriteria).Methods("POST")
+	router.HandleFunc("/criteria/", controllers.GetCriteria).Methods("GET")
+	router.HandleFunc("/criteria/{criteriaId}", controllers.GetCriteriaById).Methods("GET")
+	router.HandleFunc("/criteria/{criteriaId}", controllers.UpdateCriteria).Methods("PUT")
+	router.HandleFunc("/criteria/{criteriaId}", controllers.DeleteCriteria).Methods("DELETE")
+
+	router.HandleFunc("/webpage/", controllers.SaveWebpageScans).Methods("POST")
+	router.HandleFunc("/webpage/", controllers.GetWebpageScan).Methods("GET")
+	router.HandleFunc("/webpage/{webpageId}", controllers.GetWebpageScanById).Methods("GET")
+	router.HandleFunc("/webpage/field/{webpageField}", controllers.GetWebpageByField).Methods("GET")
+	router.HandleFunc("/webpage/{webpageId}", controllers.UpdateWebpageScan).Methods("PUT")
+	router.HandleFunc("/webpage/{webpageId}", controllers.DeleteWebpageScan).Methods("DELETE")
+
+	router.HandleFunc("/issue/{webpageId}", controllers.AddIssue).Methods("POST")
+	router.HandleFunc("/issue/{webpageId}", controllers.GetAllIssuesforWebpageId).Methods("GET")
+	router.HandleFunc("/issue/", controllers.GetIssues).Methods("GET")
+	router.HandleFunc("/issue/{issueId}", controllers.UpdateWebpageScan).Methods("PUT")
+	router.HandleFunc("/issue/{issueId}", controllers.DeleteWebpageScan).Methods("DELETE")
+
+	router.HandleFunc("/website/", controllers.CreateWebsite).Methods("POST")
+	router.HandleFunc("/website/", controllers.GetAllWebsites).Methods("GET")
+	router.HandleFunc("/website/{websiteId}", controllers.GetWebsiteById).Methods("GET")
+	router.HandleFunc("/website/{websiteId}", controllers.UpdateWebsite).Methods("PUT")
+
+}
