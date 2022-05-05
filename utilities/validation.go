dee266b3d4074c7e9ce9bd7ac8fee60d9d253ef8
essietom <essietom@gmail.com>
2022-05-05 11:23:39 +0300

    fixing cors

--- utilities/validation.go
+++ utilities/validation.go
@@ -7,6 +7,8 @@ import (
 
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+   	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
 }
 
 func ValidationResponse(fields map[string][]string, writer http.ResponseWriter) {
